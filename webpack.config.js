e3de31a662f92681c262f73dc734a00ed4fc643c
Yufi <yfhj1990@hotmail.com>
2016-03-23 01:04:25 +0800

    test webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,24 @@
-module.exports = {
-    entry: './main.js',
-    output: {
-	path: __dirname,
-	filename: 'bundle.js'
-    },
-    module: {
-	loaders: [{
+var resolve = {
+    extensions: ['', '.js', '.jsx']
+};
+
+var jsLoader = {
 		test: /\.jsx?$/,
 		loader: "babel",
 		query: {
 		    cacheDirectory: true,
 		    presets: ["es2015", "react"]
 		}
-	    }]
-    }
-}
+};
+
+module.exports = {
+    entry: './main.js',
+    output: {
+	      path: __dirname,
+	      filename: 'bundle.js'
+    },
+    module: {
+	      loaders: [jsLoader]
+    },
+    resolve: resolve
+};
