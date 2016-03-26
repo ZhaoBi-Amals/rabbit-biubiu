1f992920e76603acb0939175d9638acd09fd0f47
Yuffiy <yfhj1990@hotmail.com>
2016-03-26 18:14:17 +0800

    Aside parts reporting.

--- Application/app.js
+++ Application/app.js
@@ -13,21 +13,28 @@ import { flexCenterStart,
 const style      = ms(flexCenterStart(), bg(0.88), fg(0.56))
 const asideStyle = ms(fillHeight(), flexItem('15rem'), bg(0.24), shadow(1))
 const bodyStyle  = ms(fillHeight(), flexItemGrow())
+const headerStyle= ms()
+const listStyle  = ms()
+const menuStyle  = ms()
 
 
 export default class App extends Component {
     render() {
 	return (
-	    <section style={style}>
-	    <aside style={asideStyle}>
-	    <ul className="listReset">
-	    <li><a href='#'>Snippet 管理</a></li>
-	    <li><a href='#'>Template 管理</a></li>
-	    <li><a href='#'>Tool 管理</a></li>
-	    </ul>
-	    </aside>
-	    <section style={bodyStyle}>Hello world</section>
-	    </section>
+		<section style={style}>
+		<aside style={asideStyle}>
+		<header style={headerStyle}>兔子的私房菜</header>
+		<nav>
+		<ul className="listReset" style={listStyle}>
+		<li><a href='#' style={menuStyle}>Snippet 管理</a></li>
+		<li><a href='#' style={menuStyle}>Template 管理</a></li>
+		<li><a href='#' style={menuStyle}>Tool 管理</a></li>
+		</ul>
+		</nav>
+		<footer></footer>
+		</aside>
+		<section style={bodyStyle}>Hello world</section>
+		</section>
 	)
     }
 }
